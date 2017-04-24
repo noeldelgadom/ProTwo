d97cdb231727fef52b0659d42298a9e9dfb1e299
Noel Delgado <noeldelgadom@gmail.com>
2017-04-24 10:42:04 -0500

    primer intento del modelo para la db

--- AppTwo/models.py
+++ AppTwo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=60)
+    url = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
+    source = models.CharField(max_length=30)
+
+    def __str__(self):
+        return 'Article # %s with title: %s' % (str(self.id), self.title)
--- ProTwo/settings.py
+++ ProTwo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ProTwo',
+    'AppTwo',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'ProTwo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hackernews_db',
+        'USER': 'hackernews_admin',
+        'PASSWORD' : '12345678',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
 
